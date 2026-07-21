c8f00bfa7ea999cfe0649250118621763629ec4a
李丁先瑞 <2081607573@qq.com>
2026-07-21 11:54:03 +0800

    Add files via upload

---
+++ 变量应用.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	int money;
+	money = 50;
+	cout << "Ð¡Ã÷Óà¶î£º" << money << endl; 
+	money = money - 5;
+	cout << "¹ºÂò±ù¼¤Áè»¨·Ñ5Ôª£¬Óà¶îÊ£Óà£º" << money << endl; 
+	money = money + 10;
+	cout << "×ªÂô±ù¼¤ÁèµÃµ½10Ôª£¬Óà¶îÊ£Óà£º" << money << endl; 
+	money = money - 2;
+	cout << "¹ºÂò²ÊÆ±»¨·Ñ2Ôª£¬Óà¶îÊ£Óà£º" << money << endl; 
+	money = money * 2;
+	cout << "²ÊÆ±ÖÐ½±½ð¶î·­±¶£¬Óà¶îÊ£Óà£º" << money << endl; 
+	return 0;
+}
