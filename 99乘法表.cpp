2cac401256686a8e20bf3a53040995a1f4bea96d
李丁先瑞 <2081607573@qq.com>
2026-07-22 12:13:21 +0800

    Add files via upload

---
+++ 99乘法表.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	int line = 1;
+	while(line <= 9)
+	{
+		int num = 1;
+		while(num <= line)
+		{
+			cout << num << "x" << line << "=" << (num * line) << "\t";
+			num++;
+		}
+		line++;
+		cout << endl;
+	}
+	return 0;
+}
